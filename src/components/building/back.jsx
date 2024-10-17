4a6928dc8e21466cecf30a8bce63bef02df0a111
procaptain <procaptain1029@gmail.com>
2024-10-17 19:29:37 +0300

    feat(build): make back wall for complex building

--- src/components/building/back.jsx
+++ src/components/building/back.jsx
@@ -4,9 +4,11 @@ import { extrudeSettings } from "../units";
 import { useLoader } from '@react-three/fiber';
 import { useSelector } from 'react-redux';
 import { BackWall } from '../walls/backWall';
+import { SideWall } from '../walls/sideWall';
 
 const Back = () => {
 
+    const buildingType = useSelector((state) => state.buildingType);
     const buildingWidth = useSelector((state) => state.width);
     const buildingLength = useSelector((state) => state.length);
     const angle = useSelector((state) => state.roofAngle);
@@ -23,12 +25,30 @@ const Back = () => {
     frontWallTexture.repeat.set(2, 3);
     frontWallTexture.rotation = Math.PI / 2;
 
+    const sideWallTexture = wallLoader.clone();
+    sideWallTexture.wrapS = THREE.RepeatWrapping;
+    sideWallTexture.wrapT = THREE.RepeatWrapping;
+    sideWallTexture.repeat.set(2, 3);
+
     return (
-        <group position={[ - buildingLength / 2, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
-            <mesh rotation={[Math.PI / 2, 0, 0]} castShadow>
-                <extrudeGeometry args={[BackWall(buildingWidth, wallHeight, roofAngle), extrudeSettings( - wallDepth)]}/>
-                <meshLambertMaterial map={frontWallTexture} bumpMap={frontWallTexture} bumpScale={0.02} side={THREE.DoubleSide} toneMapped={false} />
-            </mesh>
+        <group>
+            {buildingType === 'Simple' && 
+                <group position={[ - buildingLength / 2, 0, 0]} rotation={[0, 0, Math.PI / 2]}>
+                    <mesh rotation={[Math.PI / 2, 0, 0]} castShadow>
+                        <extrudeGeometry args={[BackWall(buildingWidth, wallHeight, roofAngle), extrudeSettings( - wallDepth)]}/>
+                        <meshLambertMaterial map={frontWallTexture} bumpMap={frontWallTexture} bumpScale={0.02} side={THREE.DoubleSide} toneMapped={false} />
+                    </mesh>
+                </group>
+            }
+            {buildingType === 'Complex' &&
+                <group position={[ - buildingWidth / 2, 0, - buildingLength - buildingWidth / 2]}>
+                    <mesh rotation={[0, - Math.PI / 2, 0]} castShadow>
+                        <extrudeGeometry args={[SideWall(buildingLength + buildingWidth, wallHeight, wallDepth), extrudeSettings(wallDepth)]}/>
+                        <meshLambertMaterial map={sideWallTexture} bumpMap={sideWallTexture} bumpScale={0.02} side={THREE.DoubleSide} toneMapped={false} />
+                    </mesh>
+                </group>
+            }
+
         </group>
     )
 }
