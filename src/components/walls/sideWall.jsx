09568082837553ab295afa35671be34e0ce3fe28
procaptain <procaptain1029@gmail.com>
2024-10-17 19:29:05 +0300

    feat(build): make right wall for complex building

--- src/components/walls/sideWall.jsx
+++ src/components/walls/sideWall.jsx
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 
-export const SideWall = (wallLength, wallHeight, wallDepth) => {
+export const SideWall = (wallLength, wallHeight) => {
     const newModel = new THREE.Shape();
     newModel.moveTo(0, 0);
     newModel.lineTo(0, wallHeight);
-    newModel.lineTo(wallLength + 2 * wallDepth , wallHeight);
-    newModel.lineTo(wallLength + 2 * wallDepth , 0);
+    newModel.lineTo(wallLength  , wallHeight);
+    newModel.lineTo(wallLength  , 0);
     newModel.closePath();
     return newModel;
 }
