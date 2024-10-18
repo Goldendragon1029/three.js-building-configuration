0b24e5d9c5285287e1d7112cccaa4cdd55c54e8e
procaptain <procaptain1029@gmail.com>
2024-10-18 09:15:19 +0300

    feat(building): build complex trim

--- src/components/building/trim.jsx
+++ src/components/building/trim.jsx
@@ -5,6 +5,7 @@ import { Pillar } from '../walls/pillar';
 
 const Trim = () => {
 
+    const buildingType = useSelector((state) => state.buildingType);
     const buildingWidth = useSelector((state) => state.width);
     const buildingLength = useSelector((state) => state.length);
 
@@ -13,30 +14,72 @@ const Trim = () => {
 
     return (
         <group>
-            <group position={[buildingLength / 2 - pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]}>
-                <mesh rotation={[ - Math.PI / 2, 0, 0]}>
-                    <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
-                    <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
-                </mesh>
-            </group>
-            <group position={[ - buildingLength / 2 + pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]} rotation={[0, Math.PI / 2, 0]}>
-                <mesh rotation={[ - Math.PI / 2, 0, 0]}>
-                    <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
-                    <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
-                </mesh>
-            </group>
-            <group position={[ - buildingLength / 2 + pillarWidth / 4, 0,  buildingWidth / 2 - pillarWidth / 4]} rotation={[0, Math.PI, 0]}>
-                <mesh rotation={[ - Math.PI / 2, 0, 0]}>
-                    <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
-                    <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
-                </mesh>
-            </group>
-            <group position={[  buildingLength / 2 - pillarWidth / 4, 0,  buildingWidth / 2 - pillarWidth / 4]} rotation={[0, - Math.PI / 2, 0]}>
-                <mesh rotation={[ - Math.PI / 2, 0, 0]}>
-                    <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
-                    <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
-                </mesh>
-            </group>
+            {buildingType === 'Simple' && <group>
+                <group position={[buildingLength / 2 - pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[ - buildingLength / 2 + pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]} rotation={[0, Math.PI / 2, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[ - buildingLength / 2 + pillarWidth / 4, 0,  buildingWidth / 2 - pillarWidth / 4]} rotation={[0, Math.PI, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[  buildingLength / 2 - pillarWidth / 4, 0,  buildingWidth / 2 - pillarWidth / 4]} rotation={[0, - Math.PI / 2, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+            </group>}
+
+            {buildingType === 'Complex' && <group>
+                <group position={[buildingLength + buildingWidth / 2 - pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[buildingWidth / 2 - pillarWidth / 4, 0, - buildingLength - buildingWidth / 2 + pillarWidth / 4]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[buildingWidth / 2 - pillarWidth / 4, 0, - buildingWidth / 2 + pillarWidth / 4]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[ - buildingWidth / 2 + pillarWidth / 4, 0, - buildingWidth / 2 - buildingLength + pillarWidth / 4]} rotation={[0, Math.PI / 2, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[ - buildingWidth / 2 + pillarWidth / 4, 0, buildingWidth / 2 - pillarWidth / 4]} rotation={[0, Math.PI, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+                <group position={[ buildingWidth / 2 + buildingLength - pillarWidth / 4, 0, buildingWidth / 2 - pillarWidth / 4]} rotation={[0, - Math.PI / 2, 0]}>
+                    <mesh rotation={[ - Math.PI / 2, 0, 0]}>
+                        <extrudeGeometry args={[Pillar(pillarWidth), extrudeSettings(wallHeight)]} />
+                        <meshStandardMaterial color={0x666666} side={THREE.DoubleSide} metalness={5} roughness={1} />
+                    </mesh>
+                </group>
+            </group>}
+            
         </group>
     )
 }
